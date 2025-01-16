a3e6f9262675b7b1acea7acef343d587760fb7d9
Ironowl1907 <facuguialtana7@gmail.com>
2025-01-16 08:37:56 -0300

    Properties panel for entities

--- zirconium-Editor/src/Panels/SceneHireachyPanel.cpp
+++ zirconium-Editor/src/Panels/SceneHireachyPanel.cpp
@@ -1,6 +1,7 @@
 #include "SceneHireachyPanel.h"
 #include "zirconium/scene/Components.h"
 
+#include "glm/gtc/type_ptr.hpp"
 #include "imgui/imgui.h"
 
 namespace zirconium {
@@ -20,6 +21,16 @@ void SceneHirearchyPanel::OnImGuiRender() {
         Entity entity{ent, m_Context.get()};
         DrawEntityNode(entity);
     }
+    if (ImGui::IsMouseDown(0) && ImGui::IsWindowHovered()) {
+        m_SelectionContext = {};
+    }
+
+    ImGui::End();
+
+    ImGui::Begin("Properties");
+    if (m_SelectionContext) {
+        DrawComponents(m_SelectionContext);
+    }
     ImGui::End();
 }
 
@@ -28,12 +39,37 @@ void SceneHirearchyPanel::DrawEntityNode(Entity entity) {
 
     ImGuiTreeNodeFlags flags =
         ((m_SelectionContext == entity) ? ImGuiTreeNodeFlags_Selected : 0) | ImGuiTreeNodeFlags_OpenOnArrow;
-    bool opened = ImGui::TreeNodeEx((void*)(uint64_t)entity, flags, tag.c_str());
+    bool opened = ImGui::TreeNodeEx((void*)(uint64_t)entity, flags, "%s", tag.c_str());
     if (ImGui::IsItemClicked()) {
         m_SelectionContext = entity;
     }
     if (opened) {
         ImGui::TreePop();
+    }
+}
+
+void SceneHirearchyPanel::DrawComponents(Entity ent) {
+    if (ent.HasComponent<TagComponent>()) {
+        auto& tag = ent.GetComponent<TagComponent>().Tag;
+
+        char buffer[256];
+        memset(buffer, 0, sizeof(buffer));
+        strcpy(buffer, tag.c_str());
+
+        if (ImGui::InputText("Tag", buffer, sizeof(buffer))) {
+            tag = std::string(buffer);
+        }
+    }
+
+    if (ent.HasComponent<TransformComponent>()) {
+        if (ImGui::TreeNodeEx((const void*)typeid(TransformComponent).hash_code(), ImGuiTreeNodeFlags_DefaultOpen,
+                              "Transform")) {
+
+            auto& transform = ent.GetComponent<TransformComponent>().Transform;
+            ImGui::DragFloat3("Position", glm::value_ptr(transform[3]), 0.1f);
+
+            ImGui::TreePop();
+        }
     }
 }
 } // namespace zirconium
