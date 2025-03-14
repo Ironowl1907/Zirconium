2fb73ed6aaf3c8d4aa801bb986852160bae6f908
Ironowl1907 <facuguialtana7@gmail.com>
2025-03-14 08:38:20 -0300

    Better camera controls2

--- zirconium/src/zirconium/Renderer/EditorCamera.cpp
+++ zirconium/src/zirconium/Renderer/EditorCamera.cpp
@@ -56,19 +56,6 @@ float EditorCamera::ZoomSpeed() const {
 }
 
 void EditorCamera::OnUpdate(TimeStep ts) {
-    // if (Input::IsKeyPressed(ZR_KEY_LEFT_ALT)) {
-    //     const glm::vec2& mouse{Input::GetMouseX(), Input::GetMouseY()};
-    //     glm::vec2 delta = (mouse - m_InitialMousePosition) * 0.003f;
-    //     m_InitialMousePosition = mouse;
-
-    //     if (Input::IsMouseButtonPressed(ZR_MOUSE_BUTTON_MIDDLE))
-    //         OnMousePan(delta);
-    //     else if (Input::IsMouseButtonPressed(ZR_MOUSE_BUTTON_LEFT))
-    //         OnMouseRotate(delta);
-    //     else if (Input::IsMouseButtonPressed(ZR_MOUSE_BUTTON_RIGHT))
-    //         OnZoom(delta.y);
-    // }
-
     const glm::vec2& mouse{Input::GetMouseX(), Input::GetMouseY()};
     glm::vec2 delta = (mouse - m_InitialMousePosition) * 0.003f;
     m_InitialMousePosition = mouse;
