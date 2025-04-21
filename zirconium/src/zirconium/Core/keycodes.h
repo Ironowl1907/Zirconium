a5533d9ecb929c6610692111a104c96bc884d5be
Ironowl1907 <facuguialtana7@gmail.com>
2025-04-21 19:55:13 -0300

    Updating Keycodes

--- zirconium/src/zirconium/Core/keycodes.h
+++ zirconium/src/zirconium/Core/keycodes.h
@@ -1,129 +1,129 @@
 #pragma once
+#include <cstdint>
 
-// From GLFW3 keycodes
+enum ZrKey : uint32_t {
+    ZR_KEY_UNKNOWN = 0,
+    /* Printable keys */
+    ZR_KEY_SPACE = 32,
+    ZR_KEY_APOSTROPHE = 39, /* ' */
+    ZR_KEY_COMMA = 44,      /* , */
+    ZR_KEY_MINUS = 45,      /* - */
+    ZR_KEY_PERIOD = 46,     /* . */
+    ZR_KEY_SLASH = 47,      /* / */
+    ZR_KEY_0 = 48,
+    ZR_KEY_1 = 49,
+    ZR_KEY_2 = 50,
+    ZR_KEY_3 = 51,
+    ZR_KEY_4 = 52,
+    ZR_KEY_5 = 53,
+    ZR_KEY_6 = 54,
+    ZR_KEY_7 = 55,
+    ZR_KEY_8 = 56,
+    ZR_KEY_9 = 57,
+    ZR_KEY_SEMICOLON = 59, /* ; */
+    ZR_KEY_EQUAL = 61,     /* = */
+    ZR_KEY_A = 65,
+    ZR_KEY_B = 66,
+    ZR_KEY_C = 67,
+    ZR_KEY_D = 68,
+    ZR_KEY_E = 69,
+    ZR_KEY_F = 70,
+    ZR_KEY_G = 71,
+    ZR_KEY_H = 72,
+    ZR_KEY_I = 73,
+    ZR_KEY_J = 74,
+    ZR_KEY_K = 75,
+    ZR_KEY_L = 76,
+    ZR_KEY_M = 77,
+    ZR_KEY_N = 78,
+    ZR_KEY_O = 79,
+    ZR_KEY_P = 80,
+    ZR_KEY_Q = 81,
+    ZR_KEY_R = 82,
+    ZR_KEY_S = 83,
+    ZR_KEY_T = 84,
+    ZR_KEY_U = 85,
+    ZR_KEY_V = 86,
+    ZR_KEY_W = 87,
+    ZR_KEY_X = 88,
+    ZR_KEY_Y = 89,
+    ZR_KEY_Z = 90,
+    ZR_KEY_LEFT_BRACKET = 91,  /* [ */
+    ZR_KEY_BACKSLASH = 92,     /* \ */
+    ZR_KEY_RIGHT_BRACKET = 93, /* ] */
+    ZR_KEY_GRAVE_ACCENT = 96,  /* ` */
+    ZR_KEY_WORLD_1 = 161,      /* non-US #1 */
+    ZR_KEY_WORLD_2 = 162,      /* non-US #2 */
 
-#define ZR_KEY_UNKNOWN -1
-
-/* Printable keys */
-#define ZR_KEY_SPACE 32
-#define ZR_KEY_APOSTROPHE 39 /* ' */
-#define ZR_KEY_COMMA 44      /* , */
-#define ZR_KEY_MINUS 45      /* - */
-#define ZR_KEY_PERIOD 46     /* . */
-#define ZR_KEY_SLASH 47      /* / */
-#define ZR_KEY_0 48
-#define ZR_KEY_1 49
-#define ZR_KEY_2 50
-#define ZR_KEY_3 51
-#define ZR_KEY_4 52
-#define ZR_KEY_5 53
-#define ZR_KEY_6 54
-#define ZR_KEY_7 55
-#define ZR_KEY_8 56
-#define ZR_KEY_9 57
-#define ZR_KEY_SEMICOLON 59 /* ; */
-#define ZR_KEY_EQUAL 61     /* = */
-#define ZR_KEY_A 65
-#define ZR_KEY_B 66
-#define ZR_KEY_C 67
-#define ZR_KEY_D 68
-#define ZR_KEY_E 69
-#define ZR_KEY_F 70
-#define ZR_KEY_G 71
-#define ZR_KEY_H 72
-#define ZR_KEY_I 73
-#define ZR_KEY_J 74
-#define ZR_KEY_K 75
-#define ZR_KEY_L 76
-#define ZR_KEY_M 77
-#define ZR_KEY_N 78
-#define ZR_KEY_O 79
-#define ZR_KEY_P 80
-#define ZR_KEY_Q 81
-#define ZR_KEY_R 82
-#define ZR_KEY_S 83
-#define ZR_KEY_T 84
-#define ZR_KEY_U 85
-#define ZR_KEY_V 86
-#define ZR_KEY_W 87
-#define ZR_KEY_X 88
-#define ZR_KEY_Y 89
-#define ZR_KEY_Z 90
-#define ZR_KEY_LEFT_BRACKET 91  /* [ */
-#define ZR_KEY_BACKSLASH 92     /* \ */
-#define ZR_KEY_RIGHT_BRACKET 93 /* ] */
-#define ZR_KEY_GRAVE_ACCENT 96  /* ` */
-#define ZR_KEY_WORLD_1 161      /* non-US #1 */
-#define ZR_KEY_WORLD_2 162      /* non-US #2 */
-
-/* Function keys */
-#define ZR_KEY_ESCAPE 256
-#define ZR_KEY_ENTER 257
-#define ZR_KEY_TAB 258
-#define ZR_KEY_BACKSPACE 259
-#define ZR_KEY_INSERT 260
-#define ZR_KEY_DELETE 261
-#define ZR_KEY_RIGHT 262
-#define ZR_KEY_LEFT 263
-#define ZR_KEY_DOWN 264
-#define ZR_KEY_UP 265
-#define ZR_KEY_PAGE_UP 266
-#define ZR_KEY_PAGE_DOWN 267
-#define ZR_KEY_HOME 268
-#define ZR_KEY_END 269
-#define ZR_KEY_CAPS_LOCK 280
-#define ZR_KEY_SCROLL_LOCK 281
-#define ZR_KEY_NUM_LOCK 282
-#define ZR_KEY_PRINT_SCREEN 283
-#define ZR_KEY_PAUSE 284
-#define ZR_KEY_F1 290
-#define ZR_KEY_F2 291
-#define ZR_KEY_F3 292
-#define ZR_KEY_F4 293
-#define ZR_KEY_F5 294
-#define ZR_KEY_F6 295
-#define ZR_KEY_F7 296
-#define ZR_KEY_F8 297
-#define ZR_KEY_F9 298
-#define ZR_KEY_F10 299
-#define ZR_KEY_F11 300
-#define ZR_KEY_F12 301
-#define ZR_KEY_F13 302
-#define ZR_KEY_F14 303
-#define ZR_KEY_F15 304
-#define ZR_KEY_F16 305
-#define ZR_KEY_F17 306
-#define ZR_KEY_F18 307
-#define ZR_KEY_F19 308
-#define ZR_KEY_F20 309
-#define ZR_KEY_F21 310
-#define ZR_KEY_F22 311
-#define ZR_KEY_F23 312
-#define ZR_KEY_F24 313
-#define ZR_KEY_F25 314
-#define ZR_KEY_KP_0 320
-#define ZR_KEY_KP_1 321
-#define ZR_KEY_KP_2 322
-#define ZR_KEY_KP_3 323
-#define ZR_KEY_KP_4 324
-#define ZR_KEY_KP_5 325
-#define ZR_KEY_KP_6 326
-#define ZR_KEY_KP_7 327
-#define ZR_KEY_KP_8 328
-#define ZR_KEY_KP_9 329
-#define ZR_KEY_KP_DECIMAL 330
-#define ZR_KEY_KP_DIVIDE 331
-#define ZR_KEY_KP_MULTIPLY 332
-#define ZR_KEY_KP_SUBTRACT 333
-#define ZR_KEY_KP_ADD 334
-#define ZR_KEY_KP_ENTER 335
-#define ZR_KEY_KP_EQUAL 336
-#define ZR_KEY_LEFT_SHIFT 340
-#define ZR_KEY_LEFT_CONTROL 341
-#define ZR_KEY_LEFT_ALT 342
-#define ZR_KEY_LEFT_SUPER 343
-#define ZR_KEY_RIGHT_SHIFT 344
-#define ZR_KEY_RIGHT_CONTROL 345
-#define ZR_KEY_RIGHT_ALT 346
-#define ZR_KEY_RIGHT_SUPER 347
-#define ZR_KEY_MENU 348
+    /* Function keys */
+    ZR_KEY_ESCAPE = 256,
+    ZR_KEY_ENTER = 257,
+    ZR_KEY_TAB = 258,
+    ZR_KEY_BACKSPACE = 259,
+    ZR_KEY_INSERT = 260,
+    ZR_KEY_DELETE = 261,
+    ZR_KEY_RIGHT = 262,
+    ZR_KEY_LEFT = 263,
+    ZR_KEY_DOWN = 264,
+    ZR_KEY_UP = 265,
+    ZR_KEY_PAGE_UP = 266,
+    ZR_KEY_PAGE_DOWN = 267,
+    ZR_KEY_HOME = 268,
+    ZR_KEY_END = 269,
+    ZR_KEY_CAPS_LOCK = 280,
+    ZR_KEY_SCROLL_LOCK = 281,
+    ZR_KEY_NUM_LOCK = 282,
+    ZR_KEY_PRINT_SCREEN = 283,
+    ZR_KEY_PAUSE = 284,
+    ZR_KEY_F1 = 290,
+    ZR_KEY_F2 = 291,
+    ZR_KEY_F3 = 292,
+    ZR_KEY_F4 = 293,
+    ZR_KEY_F5 = 294,
+    ZR_KEY_F6 = 295,
+    ZR_KEY_F7 = 296,
+    ZR_KEY_F8 = 297,
+    ZR_KEY_F9 = 298,
+    ZR_KEY_F10 = 299,
+    ZR_KEY_F11 = 300,
+    ZR_KEY_F12 = 301,
+    ZR_KEY_F13 = 302,
+    ZR_KEY_F14 = 303,
+    ZR_KEY_F15 = 304,
+    ZR_KEY_F16 = 305,
+    ZR_KEY_F17 = 306,
+    ZR_KEY_F18 = 307,
+    ZR_KEY_F19 = 308,
+    ZR_KEY_F20 = 309,
+    ZR_KEY_F21 = 310,
+    ZR_KEY_F22 = 311,
+    ZR_KEY_F23 = 312,
+    ZR_KEY_F24 = 313,
+    ZR_KEY_F25 = 314,
+    ZR_KEY_KP_0 = 320,
+    ZR_KEY_KP_1 = 321,
+    ZR_KEY_KP_2 = 322,
+    ZR_KEY_KP_3 = 323,
+    ZR_KEY_KP_4 = 324,
+    ZR_KEY_KP_5 = 325,
+    ZR_KEY_KP_6 = 326,
+    ZR_KEY_KP_7 = 327,
+    ZR_KEY_KP_8 = 328,
+    ZR_KEY_KP_9 = 329,
+    ZR_KEY_KP_DECIMAL = 330,
+    ZR_KEY_KP_DIVIDE = 331,
+    ZR_KEY_KP_MULTIPLY = 332,
+    ZR_KEY_KP_SUBTRACT = 333,
+    ZR_KEY_KP_ADD = 334,
+    ZR_KEY_KP_ENTER = 335,
+    ZR_KEY_KP_EQUAL = 336,
+    ZR_KEY_LEFT_SHIFT = 340,
+    ZR_KEY_LEFT_CONTROL = 341,
+    ZR_KEY_LEFT_ALT = 342,
+    ZR_KEY_LEFT_SUPER = 343,
+    ZR_KEY_RIGHT_SHIFT = 344,
+    ZR_KEY_RIGHT_CONTROL = 345,
+    ZR_KEY_RIGHT_ALT = 346,
+    ZR_KEY_RIGHT_SUPER = 347,
+    ZR_KEY_MENU = 348
+};
