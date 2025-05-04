c4c49695db13a3d6cc8be53c566dd0ff81da74d2
Ironowl1907 <facuguialtana7@gmail.com>
2025-05-04 10:55:53 -0300

    Non-tested project file implementation

--- zirconium/src/zirconium/Project/ProjectFile.cpp
+++ zirconium/src/zirconium/Project/ProjectFile.cpp
@@ -1,8 +1,32 @@
 #include "ProjectFile.h"
 #include "zrpch.h"
 
-#include <fstream>
-#include <sstream>
+#include <memory>
 #include <yaml-cpp/yaml.h>
 
-namespace zirconium {} // namespace zirconium
+namespace zirconium {
+
+Ref<Project> Project::New(const std::filesystem::path& path) {
+    s_CurrentProject = std::make_shared<Project>();
+    return s_CurrentProject;
+}
+Ref<Project> Project::Load(const std::filesystem::path& path) {
+    Ref<Project> project = std::make_shared<Project>();
+
+    if (ProjectSerializer::Serialize(path)) {
+        project->m_ProjectDirectory = path.parent_path();
+        s_CurrentProject = project;
+        return s_CurrentProject;
+    }
+    return nullptr;
+}
+
+bool Project::SaveCurrent(const std::filesystem::path& path) {
+    if (ProjecSerializer::Serialize(path)) {
+        s_CurrentProject->m_ProjectDirectory = path.parent_path();
+        return true;
+    }
+
+    return false;
+}
+} // namespace zirconium
--- zirconium/src/zirconium/Project/ProjectFile.h
+++ zirconium/src/zirconium/Project/ProjectFile.h
@@ -15,31 +15,31 @@ struct ProjectConfig {
 class Project {
 public:
     static std::string GetProjectName() {
-        return m_CurrentProject->m_ProjectConfig.Name;
+        return s_CurrentProject->m_ProjectConfig.Name;
     }
     static std::filesystem::path GetStarterScene() {
-        return m_CurrentProject->m_ProjectConfig.StarterScene;
+        return s_CurrentProject->m_ProjectConfig.StarterScene;
     }
     static std::filesystem::path GetAssetPath() {
-        return m_CurrentProject->m_ProjectConfig.AssetPath;
+        return s_CurrentProject->m_ProjectConfig.AssetPath;
     }
 
     static Ref<Project> GetActive() {
-        return m_CurrentProject;
+        return s_CurrentProject;
     }
 
     ProjectConfig* GetConfing() {
         return &m_ProjectConfig;
     }
 
-    static void New(const std::filesystem::path& path);
-    static void Load(const std::filesystem::path& path);
-    static void SaveCurrent(const std::filesystem::path& path);
+    static Ref<Project> New(const std::filesystem::path& path);
+    static Ref<Project> Load(const std::filesystem::path& path);
+    static bool SaveCurrent(const std::filesystem::path& path);
 
 private:
 private:
     ProjectConfig m_ProjectConfig;
-    static Ref<Project> m_CurrentProject;
+    static Ref<Project> s_CurrentProject;
     std::filesystem::path m_ProjectDirectory;
 };
 
